733f0aa0e3b4423cdde8b3a3ae32cf65f4342d07
anushkachouhan029 <anushkachouhan029>
2023-01-26 21:14:21 +0530

    added new project

---
+++ UC1_SadOrHappy/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProg
+{
+    public class MoodAnalyser
+    {
+        public string message;
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public MoodAnalyser()
+        {
+
+        }
+        /// <summary>
+        /// Parameterised Constructor
+        /// </summary>
+        public MoodAnalyser(string message)
+        {
+            this.message = message.ToUpper();
+        }
+        public string AnalyseMoodWithoutConstructor(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+                return "SAD";
+
+            else return "HAPPY";
+
+        }
+        public string AnalyseMood()
+        {
+            if (message.ToLower().Contains("sad"))
+                return "SAD";
+
+            else return "HAPPY";
+        }
+
+    }
+}
+Footer
